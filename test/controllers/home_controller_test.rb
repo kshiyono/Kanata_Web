a5eaf25012612169cb53fb24cf9caf3d65419822
kshiyono <k.shiyo.no@gmail.com>
2019-11-24 17:12:15 +0900

    [add] Kanata_Web - ルートルーティングのテスト(修正済のためテストのみ)

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,6 +6,11 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Kanata ToDo管理ツール"
   end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get index" do
     get home_index_url
     assert_response :success
