dabd2c127a69282241fec9319ed768e14dd3b932
kshiyono <k.shiyo.no@gmail.com>
2019-11-24 15:34:15 +0900

    [add] Kanata_Web - 静的ページのタブタイトルのリファクタリング

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')
+    base_title = "Kanata ToDo管理ツール"
+    page_title.empty? ? base_title : page_title + " | " + base_title
+  end
 end
