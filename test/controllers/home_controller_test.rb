71a063434a8d81347449ff1443507c3cc36fa9f3
kshiyono <k.shiyo.no@gmail.com>
2019-11-24 17:10:11 +0900

    [fix] Kanata_Web - ホームページのテスト(タイトル含む)および修正

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Kanata ToDo管理ツール"
+  end
+
   test "should get index" do
     get home_index_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
 end
