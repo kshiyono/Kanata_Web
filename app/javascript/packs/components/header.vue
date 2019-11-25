e15343fbe5039b66aed902450b2d2e2cd2e4c2ec
kshiyono <k.shiyo.no@gmail.com>
2019-11-25 22:31:30 +0900

    [add] Kanata_Web - ヘッダーのコンポーネントの新規作成

---
+++ app/javascript/packs/components/header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+      <a class="title nav-link" href="#">Kanata</a>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Help</a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-lsink" href="#">Login</a>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.title {
+  float: left;
+  margin-right: 10px;
+  font-size: 1.7em;
+  color: #fff;
+  letter-spacing: -1px;
+  padding-top: 9px;
+  font-weight: bold;
+}
+</style>
