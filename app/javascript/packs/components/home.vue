2aa4f984a96824c5f1881eec17bcca8a46cf6977
kshiyono <k.shiyo.no@gmail.com>
2019-12-06 23:03:45 +0900

    [fix] Kanata_Web - 不要コードの排除・微小修正

--- app/javascript/packs/components/home.vue
+++ app/javascript/packs/components/home.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="center jumbotron">
-    <h1>Welcome to the Kanata</h1>
+  <body>
+    <div class="center jumbotron">
+      <h1>Welcome to the Kanata</h1>
 
-    <h2>
-      This is the home page for the Kanata
-    </h2>
+      <h2>
+        This is the home page for the Kanata
+      </h2>
 
-    <router-link to="/signup">
-      <button class="btn btn-lg btn-primary">sigh up now!!</button>
-    </router-link>
-  </div>
+      <router-link to="/signup">
+        <button class="btn btn-lg btn-primary">sigh up now!!</button>
+      </router-link>
+    </div>
+  </body>
 </template>
 
 <script>
@@ -25,4 +27,50 @@
   padding-top: 9px;
   font-weight: bold;
 }
+
+body {
+  padding-top: 60px;
+}
+
+section {
+  overflow: auto;
+}
+
+textarea {
+  resize: vertical;
+}
+
+.center {
+  text-align: center;
+}
+
+.center h1 {
+  margin-bottom: 10px;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  line-height: 1;
+}
+
+h1 {
+  font-size: 3em;
+  letter-spacing: -2px;
+  margin-bottom: 30px;
+  text-align: center;
+}
+
+h2 {
+  font-size: 1.2em;
+  letter-spacing: -1px;
+  margin-bottom: 30px;
+  text-align: center;
+  font-weight: normal;
+  color: #777;
+}
+
+p {
+  font-size: 1.1em;
+  line-height: 1.7em;
+}
+
 </style>
--- app/javascript/packs/components/users_list_page.vue
+++ app/javascript/packs/components/users_list_page.vue
@@ -1,20 +1,22 @@
 <template>
-  <div id="app">
-    <table>
-      <tbody>
-        <tr>
-          <th>ID</th>
-          <th>name</th>
-          <th>email</th>
-        </tr>
-        <tr v-for="user in users" :key="user.id">
-          <td>{{ user.id }}</td>
-          <td>{{ user.name }}</td>
-          <td>{{ user.email }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <body>
+    <div id="app">
+      <table>
+        <tbody>
+          <tr>
+            <th>ID</th>
+            <th>name</th>
+            <th>email</th>
+          </tr>
+          <tr v-for="user in users" :key="user.id">
+            <td>{{ user.id }}</td>
+            <td>{{ user.name }}</td>
+            <td>{{ user.email }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </body>
 </template>
 
 <script>
@@ -35,8 +37,59 @@ export default {
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
+<style scoped>
+.title {
+  float: left;
+  margin-right: 10px;
+  font-size: 1.7em;
+  color: #fff;
+  letter-spacing: -1px;
+  padding-top: 9px;
+  font-weight: bold;
+}
+
+body {
+  padding-top: 60px;
+}
+
+section {
+  overflow: auto;
+}
+
+textarea {
+  resize: vertical;
+}
+
+.center {
   text-align: center;
+}
+
+.center h1 {
+  margin-bottom: 10px;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  line-height: 1;
+}
+
+h1 {
+  font-size: 3em;
+  letter-spacing: -2px;
+  margin-bottom: 30px;
+  text-align: center;
+}
+
+h2 {
+  font-size: 1.2em;
+  letter-spacing: -1px;
+  margin-bottom: 30px;
+  text-align: center;
+  font-weight: normal;
+  color: #777;
+}
+
+p {
+  font-size: 1.1em;
+  line-height: 1.7em;
 }
 </style>
