8df3ae4ccca85d82e62782ae25e1229aeed38903
kshiyono <k.shiyo.no@gmail.com>
2019-12-11 23:20:08 +0900

    [add] Kanata_Web - Vuetifyの導入

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -1,13 +1,27 @@
-import Vue from 'vue/dist/vue.esm.js'
+import Vue from 'vue'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/dist/vuetify.min.css'
+import Vuetify from "vuetify"
+import "vuetify/dist/vuetify.min.css"
 import Router from './router/router'
 import Header from './components/header.vue'
 import Footer from './components/footer.vue'
 
-var app = new Vue({
-    router: Router,
-    el: '#app',
-    components: {
-    'navbar': Header,
-    'navfoo': Footer,
-  }
+Vue.use(Vuetify);
+const vuetify = new Vuetify();
+
+document.addEventListener("DOMContentLoaded", () => {
+  var app = new Vue({
+      router: Router,
+      el: '#app',
+      vuetify,
+      components: {
+      'navbar': Header,
+      'navfoo': Footer,
+    }
+  }).$mount();
+  document.body.appendChild(app.$el);
+
+  console.log(app);
 });
