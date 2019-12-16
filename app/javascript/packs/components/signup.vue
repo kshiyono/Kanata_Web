069406bc5a3de9e2d9d09ed3b921936bcefa5dcb
kshiyono <k.shiyo.no@gmail.com>
2019-12-16 22:53:25 +0900

    [fix] Kanata_Web - SignUpページの修正(Vuetify)

--- app/javascript/packs/components/signup.vue
+++ app/javascript/packs/components/signup.vue
@@ -1,32 +1,112 @@
 <template>
   <body>
-    <div v-if="errors.length != 0">
-      <ul v-for="e in errors" :key="e">
-        <li><font color="red">{{ e }}</font></li>
-      </ul>
-    </div>
-    <h1>Sign up</h1>
-    <div class="row">
-      <a class="col s10 m11">
-        <input v-model="name" class="form-control" placeholder="Add your Name"
-         size="200" style="width:500px;">
-      </a>
-      <a class="col s10 m11">
-        <input v-model="email" class="form-control" placeholder="Email"
-        type="email" size="200" style="width:500px;">
-      </a>
-      <a class="col s10 m11">
-        <input v-model="password" class="form-control" placeholder="Password"
-        type="password" size="200" style="width:500px;">
-      </a>
-      <a class="col s10 m11">
-        <input v-model="password_confirmation" class="form-control" placeholder="Confirmation"
-        type="password" size="200" style="width:500px;">
-      </a>
-    </div>
-    <div class="button_wrapper">
-      <button v-on:click="createUser" class="btn btn-lg btn-primary">Create my account</button>
-    </div>
+    <v-app>
+      <div v-if="errors.length != 0">
+        <ul v-for="e in errors" :key="e">
+          <v-layout wrap justify-center>
+            <v-col cols="7" sm="7" md="7">
+              <v-alert
+                border="right"
+                colored-border
+                type="error"
+                elevation="2"
+              >
+                {{ e }}
+              </v-alert>
+            </v-col>
+          </v-layout>
+        </ul>
+      </div>
+
+    <br>
+    <br>
+    <form>
+      <v-layout wrap justify-center>
+        <v-col cols="5" sm="5" md="5">
+          <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+          >
+            <div class="display-3 teal--text">Kanata</div>
+          </v-row>
+        </v-col>
+      </v-layout>
+        <v-container fluid>
+          <v-layout wrap justify-center>
+            <v-flex xs12 sm6 md6 text-center my-5>
+              <v-card
+                class="mx-auto"
+                color="#E8F5E9"
+                width="500"
+                outlined
+              >
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title class="blue-grey--text headline">Your Personal Infomation</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-layout wrap justify-center>
+                  <v-col cols="10" sm="10" md="10">
+                    <v-text-field
+                      v-model="name"
+                      label="Add your Name"
+                      outlined
+                      color="teal"
+                    ></v-text-field>
+                  </v-col>
+
+                  <v-col cols="10" sm="10" md="10">
+                    <v-text-field
+                      v-model="email"
+                      label="Email"
+                      outlined
+                      color="teal"
+                      type="email"
+                    ></v-text-field>
+                  </v-col>
+
+                  <v-col cols="10" sm="10" md="10">
+                    <v-text-field
+                      v-model="password"
+                      label="Password"
+                      outlined
+                      color="teal"
+                      type="password"
+                    ></v-text-field>
+                  </v-col>
+
+                  <v-col cols="10" sm="10" md="10">
+                    <v-text-field
+                      v-model="password_confirmation"
+                      label="Confirmation"
+                      outlined
+                      color="teal"
+                      type="password"
+                    ></v-text-field>
+                  </v-col>
+
+                </v-layout>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+
+      </v-layout wrap justify-center>
+    </form>
+
+      <v-divider></v-divider>
+
+      <v-layout wrap justify-center>
+        <v-flex xs12 sm12 md12 text-center my-5>
+          <v-btn v-on:click="createUser" x-large depressed rounded class="ma-2" outlined color="teal">
+            Create your account!
+          </v-btn>
+        </v-flex>
+      </v-layout>
+
+      <v-divider></v-divider>
+    </v-app>
   </body>
 </template>
 
@@ -63,23 +143,4 @@ export default {
 </script>
 
 <style scoped>
-
-h1, h2, h3, h4, h5, h6 {
-  line-height: 1;
-}
-
-h1 {
-  font-size: 3em;
-  letter-spacing: -2px;
-  margin-bottom: 30px;
-  text-align: center;
-}
-
-a {
-  text-align: center;
-}
-
-.button_wrapper {
-  text-align: center;
-}
 </style>
