bd0942cb32e4655549171da1832b89180b86c4b4
kshiyono <k.shiyo.no@gmail.com>
2019-12-18 22:13:20 +0900

    [fix] Kanata_Web - セッション用のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  get  '/signup',  to: 'home#index'
-  get  '/users',  to: 'home#index'#zantei
-  get  '/users/1',  to: 'home#index'#zantei
-  get  'sessions/new'
+  root   'home#index'
+  get    '/signup',  to: 'home#index'#zantei
+  get    '/users',  to: 'home#index'#zantei
+  get    '/users/1',  to: 'home#index'#zantei
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   namespace :api, {format: 'json'} do
     namespace :v1 do
       resources :users, only: [:index, :show, :create]
