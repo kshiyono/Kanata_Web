3d2a0ac5234edc75a1eda49bae4cbc9597e2f245
kshiyono <k.shiyo.no@gmail.com>
2019-12-19 19:56:20 +0900

    [add] Kanata_Web - ログインフォームの新規作成

--- app/javascript/packs/components/login.vue
+++ app/javascript/packs/components/login.vue
@@ -1,17 +1,119 @@
 <template>
   <body>
     <v-app>
+      <div v-if="errors.length != 0">
+        <ul v-for="e in errors" :key="e">
+          <v-layout wrap justify-center>
+            <v-col cols="7" sm="7" md="7">
+              <v-alert
+                border="right"
+                colored-border
+                type="error"
+                elevation="2"
+              >
+                {{ e }}
+              </v-alert>
+            </v-col>
+          </v-layout>
+        </ul>
+      </div>
 
+    <br>
+    <br>
+
+    <form>
+      <v-layout wrap justify-center>
+        <v-col cols="5" sm="5" md="5">
+          <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+          >
+            <div class="display-3 teal--text">Kanata</div>
+          </v-row>
+        </v-col>
+      </v-layout>
+        <v-container fluid>
+          <v-layout wrap justify-center>
+            <v-flex xs12 sm6 md6 text-center my-5>
+              <v-card
+                class="mx-auto"
+                color="#E8F5E9"
+                width="500"
+                outlined
+              >
+                <br>
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title class="blue-grey--text headline">Login</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-layout wrap justify-center>
+
+                  <v-col cols="10" sm="10" md="10">
+                    <v-text-field
+                      v-model="email"
+                      label="Add your Email"
+                      outlined
+                      color="teal"
+                      type="email"
+                    ></v-text-field>
+                  </v-col>
+
+                  <v-col cols="10" sm="10" md="10">
+                    <v-text-field
+                      v-model="password"
+                      label="Password"
+                      outlined
+                      color="teal"
+                      type="password"
+                    ></v-text-field>
+                  </v-col>
+                </v-layout>
+
+                <v-divider ></v-divider>
+
+                <br>
+                <router-link to="/signup" class="blue-grey--text">
+                  <div>
+                    New user? Create your account!
+                    <v-icon>mdi-account</v-icon>
+                  </div>
+                </router-link>
+                <br>
+
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+
+      </v-layout wrap justify-center>
+    </form>
+
+      <v-divider></v-divider>
+
+      <v-layout wrap justify-center>
+        <v-flex xs12 sm12 md12 text-center my-5>
+          <v-btn v-on:click="createUser" x-large depressed rounded class="ma-2" outlined color="teal">
+            Login
+          </v-btn>
+        </v-flex>
+      </v-layout>
+
+      <v-divider></v-divider>
     </v-app>
   </body>
 </template>
 
 <script>
+export default {
+  data: () => ({
+      email:                 '',
+      password:              '',
+      errors:                ''
+  })
+}
 </script>
 
 <style scoped>
-a {
-  text-decoration: none;
-  font-family: "游ゴシック","arial unicode ms",sans-serif;
-}
 </style>
