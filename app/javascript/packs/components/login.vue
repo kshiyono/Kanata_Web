9a627d9b56a5c5a5aa5689d68ee109f579275de7
kshiyono <k.shiyo.no@gmail.com>
2019-12-19 17:34:58 +0900

    [add] Kanata_Web - ログインルーティング・ページの追加(フロントエンド)

---
+++ app/javascript/packs/components/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <body>
+    <v-app>
+
+    </v-app>
+  </body>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  font-family: "游ゴシック","arial unicode ms",sans-serif;
+}
+</style>
--- app/javascript/packs/router/router.js
+++ app/javascript/packs/router/router.js
@@ -4,6 +4,7 @@ import Header from '../components/header.vue'
 import Footer from '../components/footer.vue'
 import Home from '../components/home.vue'
 import SignUp from '../components/signup.vue'
+import LogIn from '../components/login.vue'
 import UsersListPage from '../components/users_list_page.vue'
 //import UserDetalePage from './components/user_detail_page.vue'
 Vue.use(VueRouter)
@@ -12,11 +13,17 @@ export default new VueRouter ({
   mode: 'history',
   routes: [
     { path: '/',
+      name: 'Home',
       component: Home  },
     { path: '/users',
+      name: 'UsersListPage',
       component: UsersListPage  },
     { path: '/signup',
+      name: 'SignUp',
       component: SignUp  },
+    { path: '/login',
+      name: 'LogIn',
+      component: LogIn  },
 //    { path: '/users/:id(\\d+)',
 //      name: 'UserDetalePage',
 //      component: UserDetalePage }
