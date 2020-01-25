ec5ce705429d7f1af6e282f47afa87cb360bc863
kshiyono <k.shiyo.no@gmail.com>
2020-01-25 23:34:19 +0900

    [fix] Kanata_Web - ユーザプロフィール更新モーダル作成

--- app/javascript/packs/components/user_detail_page.vue
+++ app/javascript/packs/components/user_detail_page.vue
@@ -29,11 +29,66 @@
                   <v-list-item-title>{{ $store.getters.getLoginUser.name }}</v-list-item-title>
                   <v-list-item-subtitle>Logged In</v-list-item-subtitle>
                 </v-list-item-content>
-                <router-link to="/users/:user.id/edit">
-                  <v-btn depressed rounded class="ma-2" outlined color="teal">
-                    Edit Profile
-                  </v-btn>
-                </router-link>
+
+                <v-row justify="center">
+                  <v-dialog v-model="showContent" width="600px">
+                    <template v-slot:activator="{ on }">
+                      <v-btn depressed rounded class="ma-2" outlined color="teal" v-on:click="openModal">
+                        プロフィールを編集
+                      </v-btn>
+                    </template>
+                    <v-toolbar dark color="secondary">
+                      <v-btn icon dark v-on:click="closeModal">
+                        <v-icon>mdi-close</v-icon>
+                      </v-btn>
+                      <v-toolbar-title>プロフィールを編集</v-toolbar-title>
+                      <v-spacer></v-spacer>
+                      <v-btn depressed rounded class="ma-2" outlined color="white" v-on:click="closeModal">
+                        保存
+                      </v-btn>
+                    </v-toolbar>
+                    <v-card>
+                      <v-layout wrap justify-center>
+                        <v-col cols="8" sm="8" md="8">
+                          <v-text-field
+                            v-model="name"
+                            label="Update your Name"
+                            color="teal"
+                          ></v-text-field>
+                        </v-col>
+
+                        <v-col cols="8" sm="8" md="8">
+                          <v-text-field
+                            v-model="email"
+                            label="Email"
+                            color="teal"
+                            type="email"
+                          ></v-text-field>
+                        </v-col>
+
+                        <v-col cols="8" sm="8" md="8">
+                          <v-text-field
+                            v-model="password"
+                            label="Password"
+                            color="teal"
+                            type="password"
+                          ></v-text-field>
+                        </v-col>
+
+                        <v-col cols="8" sm="8" md="8">
+                          <v-text-field
+                            v-model="password_confirmation"
+                            label="Confirmation"
+                            color="teal"
+                            type="password"
+                          ></v-text-field>
+                        </v-col>
+
+                      </v-layout>
+                    </v-card>
+                  </v-dialog>
+                </v-row>
+
               </v-list-item>
             </v-layout>
           </template>
@@ -48,16 +103,17 @@
 import axios from 'axios';
 
 export default {
-  data: function () {
-    return {
-      // user: {},
+  data: () => ({
+      showContent: false
+  }),
+  methods:{
+    openModal: function(){
+      this.showContent = true
+    },
+    closeModal: function(){
+      this.showContent = false
     }
-  },
-  // mounted () {
-  //   axios
-  //     .get(`/api/v1/users/${this.$route.params.id}.json`)
-  //     .then(response => (this.user = response.data))
-  // }
+  }
 }
 </script>
 
