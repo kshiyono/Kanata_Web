83cd7bcd24f1bd8ec39f5d645e9452d28afd6765
kshiyono <k.shiyo.no@gmail.com>
2020-01-26 18:21:11 +0900

    [fix] Kanata_Web - ユーザとアップロード画像の関連付け準備(マイグレーション)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_31_094904) do
+ActiveRecord::Schema.define(version: 2020_01_26_091701) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_12_31_094904) do
     t.string "password_digest"
     t.string "remember_digest"
     t.string "id_digest"
+    t.json "picture"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
